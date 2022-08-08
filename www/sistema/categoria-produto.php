f6694232183eea9953cd27a3e0d70ec9a4f57712
Geraldo Alves <92646470+alvesgeraldo@users.noreply.github.com>
2022-08-08 15:24:04 -0300

    overlay cadastro nova categoria

--- www/sistema/categoria-produto.php
+++ www/sistema/categoria-produto.php
@@ -9,30 +9,67 @@
   
   <?php require 'menu.php'; ?>
 
+  <!-- Div principal -->
   <div class="container px-5 border-start border-end">
+    <!-- Título -->
     <h2 class="py-3">Cadastro Categorias</h2>
-    <div class="container p-3 rounded bg-secondary">
+    <!-- Div form busca -->
+    <div class="container p-4 rounded bg-secondary">
       <h5 class="text-light">Filtro de busca</h5>  
-      <div class="row text-light">
-        <div class="col-6">
-          <label for="nome" class="form-label">Nome Categoria</label>
-          <input class="form-control" type="text" name="nome-categoria" id="nome">
+      <form action="script-categoria.php" method="post">
+        <div class="row text-light">
+          <div class="col-8">
+            <label for="nome" class="form-label">Nome Categoria</label>
+            <input class="form-control" type="text" name="nome-categoria" id="nome">
+          </div>
+          <div class="col-4">
+            <label for="status" class="form-label">Status</label>
+            <select class="form-select" name="status-categoria">
+              <option value="1">Ativo</option>
+              <option value="2">Inativo</option>
+            </select>
+          </div>
         </div>
-        <div class="col-3">
-          <label for="status" class="form-label">Status</label>
-          <select class="form-select">
-            <option value="1">Ativo</option>
-            <option value="2">Inativo</option>
-          </select>
+        <div class="mt-2">
+          <button class="btn btn-primary" type="submit">Buscar</button>
         </div>
-      </div>
-      <div class="mt-1">
-        <a class="btn btn-primary" href="#">Buscar</a>
-      </div>
+      </form>
     </div>
+    <!-- Botão adicionar -->
     <div class="my-2">
-        <a class="btn btn-success" href="#">Adicionar</a>
+        <a class="btn btn-success" onclick="on()">Adicionar</a>
     </div>
+
+    <!-- Div overlay adicionar categorias -->
+
+    <div id="overlay-adicionar">
+      <div class="container bg-secondary text-light rounded p-5 mt-5">
+        <div>
+          <h5>Cadastrar nova categoria</h5>
+          <form action="script-categoria.php" method="post">
+            <div class="row">
+              <div class="col-8">
+                <label for="nome" class="form-label">Nome Categoria</label>
+                <input class="form-control" type="text" name="nome-categoria" id="nome">
+              </div>
+              <div class="col-4">
+                <label for="status" class="form-label">Status</label>
+                <select class="form-select" name="status-categoria">
+                  <option value="1">Ativo</option>
+                  <option value="2">Inativo</option>
+                </select>
+              </div>
+            </div>
+            <div class="mt-2">
+              <button class="btn btn-primary" onclick="off()" type="submit">Salvar</button>
+              <a class="btn btn-danger" onclick="off()" href="categoria-produto.php">Voltar</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <!-- Div table lista -->
     <div class="container p-3 rounded bg-secondary">
       <div class="bg-light m-3">
         <table class="table">
@@ -49,31 +86,55 @@
               <th scope="row">1</th>
               <td>Pneus</td>
               <td>Ativo</td>
-              <td>Editar - Excluir</td>
+              <td>
+                <a class="btn btn-outline-success" href="#"><i class="fa-solid fa-pen-to-square"></i></a>
+                <a class="btn btn-outline-danger" href="#"><i class="fa-solid fa-trash"></i></a>
+              </td>
             </tr>
             <tr>
               <th scope="row">2</th>
               <td>Camara de Ar</td>
               <td>Ativo</td>
-              <td>Editar - Excluir</td>
+              <td>
+                <a class="btn btn-outline-success" href="#"><i class="fa-solid fa-pen-to-square"></i></a>
+                <a class="btn btn-outline-danger" href="#"><i class="fa-solid fa-trash"></i></a>
+              </td>
             </tr>
             <tr>
               <th scope="row">3</th>
               <td>Transmissão</td>
               <td>Ativo</td>
-              <td>Editar - Excluir</td>
+              <td>
+                <a class="btn btn-outline-success" href="#"><i class="fa-solid fa-pen-to-square"></i></a>
+                <a class="btn btn-outline-danger" href="#"><i class="fa-solid fa-trash"></i></a>
+              </td>
             </tr>
             <tr>
               <th scope="row">4</th>
               <td>Câmbio</td>
               <td>Ativo</td>
-              <td>Editar - Excluir</td>
+              <td>
+                <a class="btn btn-outline-success" href="#"><i class="fa-solid fa-pen-to-square"></i></a>
+                <a class="btn btn-outline-danger" href="#"><i class="fa-solid fa-trash"></i></a>
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
   </div>
+
+  <script>
+
+      function on() {
+        document.getElementById("overlay-adicionar").style.display = "block";
+      }
+
+      function off() {
+        document.getElementById("overlay-adicionar").style.display = "none";
+      } 
+
+  </script>
 
 </body>
 </html>
---
+++ www/sistema/script-categoria.php
@@ -0,0 +1,5 @@
+<?php
+
+  require '../../sistema-private/script-categoria.php';
+
+?>
